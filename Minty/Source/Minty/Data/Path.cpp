bd50b39d4e9672add3bcec07943cfa069e1f796c
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-03-08 01:36:47 -0600

    Update Path

--- Minty/Source/Minty/Data/Path.cpp
+++ Minty/Source/Minty/Data/Path.cpp
@@ -22,6 +22,21 @@ Path Minty::Path::operator/(Path const& other) const
 	return Path(*this).append(other);
 }
 
+Path Minty::Path::get_absolute() const
+{
+	return Path(std::filesystem::absolute(m_path).string().data());
+}
+
+Path Minty::Path::get_extension() const
+{
+	return Path(m_path.extension().string().data());
+}
+
+Path Minty::Path::get_relative_to(Path const& other) const
+{
+	return Path(m_path.lexically_relative(other.m_path).string().data());
+}
+
 void Minty::Path::reserve(Size const capacity)
 {
 	MINTY_ASSERT(false, "Not implemented.");
@@ -32,6 +47,26 @@ Path& Minty::Path::append(Path const& other)
 	String str = other.get_string();
 	m_path.append(str.get_data());
 	return *this;
+}
+
+Bool Minty::Path::exists(Path const& path)
+{
+	return std::filesystem::exists(path.m_path);
+}
+
+Bool Minty::Path::is_file(Path const& path)
+{
+	return std::filesystem::is_regular_file(path.m_path);
+}
+
+Bool Minty::Path::is_directory(Path const& path)
+{
+	return std::filesystem::is_directory(path.m_path);
+}
+
+Size Minty::Path::get_file_size(Path const& path)
+{
+	return std::filesystem::file_size(path.m_path);
 }
 
 std::ostream& Minty::operator<<(std::ostream& stream, Path const& path)
