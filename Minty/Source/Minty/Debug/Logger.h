4e7509491e6dfd10feee026bba0827416aa745a4
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-03-09 17:46:45 -0500

    Update Logger
    Files only

--- Minty/Source/Minty/Debug/Logger.h
+++ Minty/Source/Minty/Debug/Logger.h
@@ -1,7 +1,9 @@
 #pragma once
+#include "Minty/Core/Macro.h"
+#include "Minty/Core/ToString.h"
 #include "Minty/Core/Types.h"
 #include "Minty/Data/Path.h"
-#include <ostream>
+#include "Minty/File/PhysicalFile.h"
 
 namespace Minty
 {
@@ -13,10 +15,7 @@ namespace Minty
 #pragma region Variables
 
 	private:
-		// the stream to log to
-		std::ostream* mp_stream;
-		// if true, delete on destruction
-		Bool m_ownsStream;
+		PhysicalFile* mp_file;
 
 #pragma endregion
 
@@ -25,24 +24,12 @@ namespace Minty
 	private:
 		Logger() = delete;
 
-		Logger(std::ostream* mp_stream, Bool ownsStream)
-			: mp_stream(mp_stream)
-			, m_ownsStream(ownsStream)
-		{
-		}
-
 	public:
 		/// <summary>
 		/// Creates a new Logger that writes to the given file.
 		/// </summary>
 		/// <param name="path">The path to the file. The file is created if it does not exist.</param>
 		Logger(Path const& path);
-
-		/// <summary>
-		/// Creates a new Logger that writes to the given stream.
-		/// </summary>
-		/// <param name="stream">The stream to write to.</param>
-		Logger(std::ostream& stream);
 
 		Logger(Logger const& other) = delete;
 
@@ -51,19 +38,15 @@ namespace Minty
 		/// </summary>
 		/// <param name="other">The Logger to move.</param>
 		Logger(Logger&& other) noexcept
-			: mp_stream(other.mp_stream)
-			, m_ownsStream(other.m_ownsStream)
+			: mp_file(other.mp_file)
 		{
-			other.mp_stream = nullptr;
-			other.m_ownsStream = false;
+			other.mp_file = nullptr;
 		}
 
 		~Logger()
 		{
-			if (m_ownsStream)
-			{
-				delete mp_stream;
-			}
+			mp_file->close();
+			delete mp_file;
 		}
 
 #pragma endregion
@@ -77,10 +60,8 @@ namespace Minty
 		{
 			if (this != &other)
 			{
-				mp_stream = other.mp_stream;
-				m_ownsStream = other.m_ownsStream;
-				other.mp_stream = nullptr;
-				other.m_ownsStream = false;
+				mp_file = other.mp_file;
+				other.mp_file = nullptr;
 			}
 			return *this;
 		}
@@ -90,7 +71,54 @@ namespace Minty
 #pragma region Methods
 
 	public:
-		
+		/// <summary>
+		/// Writes nothing.
+		/// </summary>
+		void write() const {}
+
+		/// <summary>
+		/// Writes the given value(s) to the stream.
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <typeparam name="...Args">The types of additional values.</typeparam>
+		/// <param name="value">The value.</param>
+		/// <param name="...args">The additional values.</param>
+		template<typename T, typename... Args>
+		void write(T const& value, Args const&... args) const
+		{
+			mp_file->write(to_string(value));
+			write(args...);
+		}
+
+		/// <summary>
+		/// Writes a newline.
+		/// </summary>
+		void write_line() const
+		{
+			mp_file->write("\n", sizeof(Char));
+		}
+
+		/// <summary>
+		/// Writes the given value(s) to the stream and then writes a newline.
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <typeparam name="...Args">The types of additional values.</typeparam>
+		/// <param name="value">The value.</param>
+		/// <param name="...args">The additional values.</param>
+		template<typename T, typename... Args>
+		void write_line(T const& value, Args const&... args) const
+		{
+			write(value, args...);
+			write_line();
+		}
+
+		/// <summary>
+		/// Flushes the stream.
+		/// </summary>
+		void flush() const
+		{
+			mp_file->flush();
+		}
 
 #pragma endregion
 	};
