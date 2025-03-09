03c879d63b247a6c144dcbf23ac690202ed6d656
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-03-09 17:46:51 -0500

    Update Base.cpp

--- Minty/Source/Minty/Core/Base.cpp
+++ Minty/Source/Minty/Core/Base.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "Base.h"
-#include "Minty/Application/Application.h"
+#include "Minty/Context/Context.h"
 
 using namespace Minty;
 
@@ -12,8 +12,8 @@ void* Minty::allocate(Size const size, Allocator const allocator)
 	}
 
 	// using application memory manager
-	Application& app = Application::get_instance();
-	MemoryManager& memoryManager = app.get_memory_manager();
+	Context& context = Context::get_instance();
+	MemoryManager& memoryManager = context.get_memory_manager();
 	return memoryManager.allocate(size, allocator);
 }
 
@@ -26,7 +26,7 @@ void Minty::deallocate(void* const ptr, Size const size, Allocator const allocat
 	}
 
 	// using application memory manager
-	Application& app = Application::get_instance();
-	MemoryManager& memoryManager = app.get_memory_manager();
+	Context& context = Context::get_instance();
+	MemoryManager& memoryManager = context.get_memory_manager();
 	memoryManager.deallocate(ptr, size, allocator);
 }
