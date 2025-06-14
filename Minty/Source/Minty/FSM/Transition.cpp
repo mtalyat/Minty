f68733fe4e7583452d98b7e171cad02fe9527c08
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-06-14 07:42:52 -0500

    Change Transition serialization

--- Minty/Source/Minty/FSM/Transition.cpp
+++ Minty/Source/Minty/FSM/Transition.cpp
@@ -23,28 +23,28 @@ Bool Minty::Transition::evaluate(BasicScope const& scope) const
 	return true;
 }
 
-void Minty::Transition::serialize(Writer& writer) const
+void Minty::Transition::serialize(Writer& writer, String const& name) const
 {
 	FSM const* fsm = static_cast<FSM const*>(writer.get_user_data());
 	MINTY_ASSERT(fsm != nullptr, "FSM is null.");
 
 	String stateName = fsm->get_state_name(m_stateId);
-	writer.write("State", stateName);
-	writer.write("Conditions", m_conditions);
+	writer.write(name, stateName);
+	writer.write(name, m_conditions);
 }
 
-Bool Minty::Transition::deserialize(Reader& reader)
+Bool Minty::Transition::deserialize(Reader& reader, Size const index)
 {
 	FSM* fsm = static_cast<FSM*>(reader.get_user_data());
 	MINTY_ASSERT(fsm != nullptr, "FSM is null.");
 
 	String stateName;
-	if (reader.read("State", stateName))
+	if (reader.read(index, stateName))
 	{
 		// name read successfully
 		m_stateId = fsm->find_state(stateName);
 	}
-	reader.read("Conditions", m_conditions);
+	reader.read(index, m_conditions);
 
 	return true;
 }
