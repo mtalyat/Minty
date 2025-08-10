542bd3389bddd0475de6a2d6ac5c2a593e6e36f3
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-08-10 00:24:35 -0500

    Fix animator force bug

--- Minty/Source/Minty/Animation/Animator.cpp
+++ Minty/Source/Minty/Animation/Animator.cpp
@@ -39,7 +39,29 @@ UUID Minty::Animator::update(Ref<Animation> const& currentAnimation, Float const
 	// 4. Animation has completed
 	if (m_force || currentAnimation == nullptr || currentTime < 0.0f || currentTime >= currentAnimation->get_duration())
 	{
-		m_fsm.evaluate();
+		Bool result;
+
+#if defined(MINTY_DEBUG)
+		// if in debug, check for infinite loops
+		Set<UUID> visitedStates;
+#endif // MINTY_DEBUG
+
+		// if forcing, keep evaluating until no more transitions are possible
+		do {
+			result = m_fsm.evaluate();
+#if defined(MINTY_DEBUG)
+			if (result && m_fsm.has_current_state())
+			{
+				UUID id = m_fsm.get_current_state().get_value().get<UUID>();
+				if (visitedStates.contains(id))
+				{
+					MINTY_ERROR(F("Infinite loop detected in Animator FSM. State ID: {}", id));
+					break; // break out of the loop to prevent infinite recursion
+				}
+				visitedStates.add(id);
+			}
+#endif // MINTY_DEBUG
+		} while (m_force && result);	
 	}
 
 	// if no current state, return invalid ID
