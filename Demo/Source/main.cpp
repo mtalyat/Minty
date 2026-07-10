c19414c9466ca7e162498aad7e95d6c8b52e90da
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2026-07-10 16:03:11 -0500

    Add Camera to demo

--- Demo/Source/main.cpp
+++ Demo/Source/main.cpp
@@ -53,6 +53,9 @@ int main()
     renderTargetInfo.renderPass = renderPassHandle;
     renderTargetInfo.surface = surfaceHandle;
     RenderTargetHandle const renderTargetHandle = renderManager.create(renderTargetInfo);
+    CameraInfo cameraInfo{};
+    cameraInfo.renderTarget = renderTargetHandle;
+    CameraHandle const cameraHandle = renderManager.create(cameraInfo);
 
     while (windowManager.is_open(window))
     {
