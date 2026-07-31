ad018f994a126197e60f7767ad275eb7e98427d3
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2026-07-31 14:18:50 -0500

    Add a Material handle

--- Engine/Source/World/Component/SpriteComponent.hpp
+++ Engine/Source/World/Component/SpriteComponent.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Render/Sprite/Sprite.hpp"
+#include "Render/Type/Handle.hpp"
 #include "Core/Type/Color.hpp"
 #include "Core/Type/FlipState.hpp"
 
@@ -12,6 +13,12 @@ namespace Minty
          * @brief The sprite to render.
          */
         Sprite sprite = {};
+
+        /**
+         * @brief The material to use for rendering the sprite.
+         * @note If INVALID_HANDLE, the default sprite material will be used.
+         */
+        MaterialHandle materialHandle = INVALID_HANDLE;
 
         /**
          * @brief The color of the sprite.
